0fcd06053efdc022456a02c1d5d39805d1173c2b
James <james.ekasiba@student.moringaschool.com>
2023-10-10 15:20:44 -0700

    fetching API

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+var myHeaders = new Headers();
+myHeaders.append("X-Api-Key", "2ujXDP62s+wRKQKcy/7fng==PsgZKBri0ByBkKtn");
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  function fetchDogBreedDetails(dogName) {
+    fetch(`https://api.api-ninjas.com/v1/dogs?energy=3/${dogName}`)
+      .then((response) => response.text())
+      .catch((error) => console.log("error", error));
+  }
 });
